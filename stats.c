008f5e26d4a9b71843efccdaec04ad98c8abe97c
praveenmugadur <praveenmugadur5@gmail.com>
2022-05-23 12:55:33 +0530

    Update stats.c
    ; added after struct

--- stats.c
+++ stats.c
@@ -6,7 +6,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
-}
+};
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
